df1daabe3a703f2941d88e2712f9f21492d47133
Starkfielld <paulorobertoneiva2@gmail.com>
2022-10-25 20:52:01 -0300

    models e migration

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Login
+from .models import cliente
+from .models import produto
+
+admin.site.register(Login)
+admin.site.register(cliente)
+admin.site.register(produto)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+    login = models.CharField(max_length = 20)
+    passoword = models.CharField(max_length=20)
+
+class produto(models.Model):
+    nameprod = models.CharField(max_length = 20)
+    preco = models.FloatField(10)
+
+class cliente(models.Model):
+    nome = models.CharField( max_length=50)
